44fd5aaa02d15e69bac6cfe0e3e723b341dac338
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2014-04-21 18:55:21 -0400

    Added a 'collection' page, and removed 'tops' page.

--- modules/custom/example/example.js
+++ modules/custom/example/example.js
@@ -2,36 +2,50 @@
  * Implements hook_menu()
  */
 function example_menu() {
-  var items = {
-    'tops':{
-      'title':'Tops',
-      'page_callback':'example_tops_page',
-      'pageshow':'example_tops_pageshow'
-    }
+  var items = { };
+  items['collection/%/%'] = {
+    title: 'Collection',
+    page_callback: 'example_collection_page'
   };
   return items;
 }
 
 /**
- * Tops page callback.
+ * Collection page callback.
  */
-function example_tops_page() {
+function example_collection_page() {
+  
+  // Grab the collection and category from the path.
+  var collection = arg(1);
+  var collection_path = '';
+  switch (collection) {
+    case 'carry': collection_path = 'To carry'; break;
+    case 'drink': collection_path = 'To drink with'; break;
+    case 'geek': collection_path = 'To geek out'; break;
+    case 'wear': collection_path = 'To wear'; break;
+  }
+  collection_path = encodeURIComponent(collection_path);
+  var category = arg(2);
+  var category_path = 'all';
+  if (!category) { category = 'all'; }
+  category_path = encodeURIComponent(category_path);
+  
   var content = {};
   content.tops = {
     theme: 'view',
     format: 'ul',
-    path: 'tops.json',
-    row_callback: 'example_tops_page_row',
+    path: 'drupalgap/products/' + collection_path + '/' + category_path,
+    row_callback: 'example_collection_page_row',
     items: [],
-    attributes: { id: 'tops_list' },
+    attributes: { id: 'collections_list_' + collection + '_' + category },
   };
   return content;
 }
 
 /**
- * Tops row callback.
+ * Collection row callback.
  */
-function example_tops_page_row(view, row) {
+function example_collection_page_row(view, row) {
   var html = l(
     theme('image', { path: row.image }) +
       '<h2>' + row.title + '</h2>' + 
